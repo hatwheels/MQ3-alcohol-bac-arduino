8d6112c3bb0ba5096abfcad0ffefdd85f011f942
hatwheels <12772651+hatwheels@users.noreply.github.com>
2022-07-20 00:36:31 +0300

    TFSM Class: all Init(...) made private (_init(...)), added state action argument, added method set_all(...) (with defaults) to set all setters,

--- lib/Tfsm/Tfsm.cpp
+++ lib/Tfsm/Tfsm.cpp
@@ -11,13 +11,16 @@
 TFSM::TFSM(ST_STATE pStates[], size_t n)
 {
   if (this->_pStates != NULL)
-    free(_pStates);
+    free(this->_pStates);
 
   this->_n = n;
   this->_size = _n * sizeof(ST_STATE);
   this->_pStates = (ST_STATE *) malloc(_size);
-  memcpy(_pStates, pStates, _size);
-  this->init();
+  memcpy(this->_pStates, pStates, _size);
+  this->_action_arg_type = _E_ARG_TYPE_NONE;
+  memset(this->_str_action_arg, 0, sizeof(this->_str_action_arg));
+  this->_alt_transition = false;
+  this->_init();
 }
 
 TFSM::~TFSM()
@@ -27,19 +30,34 @@ TFSM::~TFSM()
   this->_n = 0;
   this->_size = 0;
   this->_state = {0};
+  this->_action_arg_type = _E_ARG_TYPE_NONE;
+  memset(this->_str_action_arg, 0, sizeof(this->_str_action_arg));
   this->_alt_transition = false;
 }
 
-void TFSM::init(void)
+void TFSM::_init(void)
 {
   this->_state = this->_pStates[0];
   this->_alt_transition = false;
 }
 
-void TFSM::init(ST_STATE state)
+void TFSM::_init(ST_STATE state)
 {
   this->_state = state;
   this->_alt_transition = false;
+  if (NULL == this->_state.action_arg)
+  {
+    switch (this->_action_arg_type)
+    {
+      case _E_ARG_TYPE_CHAR_ARRAY:
+        this->_state.action_arg = (void *) this->_str_action_arg;
+        this->_action_arg_type = _E_ARG_TYPE_NONE;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
 
 void TFSM::run(void)
@@ -47,7 +65,7 @@ void TFSM::run(void)
   if (this->_state.steps > 0)
   {
     if (this->_state.action != NULL)
-      this->_state.action();
+      this->_state.action(this->_state.action_arg);
     --this->_state.steps;
 
     return;
@@ -68,10 +86,10 @@ void TFSM::run(void)
     if (this->_state.delay_cb != NULL)
       this->_state.delay_cb();
 
-    this->init(this->_pStates[s]);
+    this->_init(this->_pStates[s]);
 
     if (this->_state.action != NULL)
-      this->_state.action();
+      this->_state.action(this->_state.action_arg);
     --this->_state.steps;
   }
 }
@@ -86,17 +104,52 @@ int32_t TFSM::get_current_steps(void)
   return this->_state.steps;
 }
 
-void TFSM::set_alt_transition(void)
-{
-  this->_alt_transition = true;
-}
-
 void TFSM::force_transition(void)
 {
   this->_state.steps = 0;
 }
 
+void TFSM::set_alt_transition(void)
+{
+  this->_alt_transition = true;
+}
+
 void TFSM::set_delay(int16_t delay)
 {
-  this->_state.delay = delay;
+  if (delay > 0)
+  {
+    this->_state.delay = delay;
+  }
+}
+
+void TFSM::set_action_arg(const char * str_action_arg)
+{
+  if (str_action_arg != NULL && strlen(str_action_arg) > 0 && strlen(str_action_arg) < 34)
+  {
+    memset(this->_str_action_arg, 0, sizeof(this->_str_action_arg));
+    strcpy(this->_str_action_arg, str_action_arg);
+    this->_action_arg_type = _E_ARG_TYPE_CHAR_ARRAY;
+  }
+}
+
+void TFSM::set_all(
+  bool alt_transition /*=false*/,
+  int16_t delay /*=-1*/,
+  bool force_transition /*=false*/,
+  const char *str_action_arg /*=NULL*/
+)
+{
+  this->set_action_arg(str_action_arg);
+
+  if (alt_transition)
+  {
+    this->set_alt_transition();
+  }
+
+  this->set_delay(delay);
+
+  if (force_transition)
+  {
+    this->force_transition();
+  }
 }
--- lib/Tfsm/Tfsm.h
+++ lib/Tfsm/Tfsm.h
@@ -32,8 +32,8 @@
  *                     current state. It is decremented at each "cycle" of the
  *                     current state.
  *            - delay: A transition delay in number of cycles. It starts being
- *                     decremented every cycle after "steps" reached 0 after.
- *                     The purpose it serves is a practical one, such as waiting
+ *                     decremented every cycle after "steps" reached 0. The
+ *                     purpose it serves is a practical one, such as waiting
  *                     for a driver to initialize after the current step, but
  *                     before the next one.
  *          Inputs: Currently there aren't any variables for inputs. External
@@ -46,6 +46,14 @@
  *                       The "primary" transition is by default triggered at the
  *          State action: "action" is a callback, where the logic/action
  *                        of the state is assigned to. It is run at every cycle.
+ *          State action argument: Parameter passed to a state action before a
+ *                        transition. It is meant for specific handling of a
+ *                        a state dependent on the previous state, instead
+ *                        of a general handling of the state no matter what the
+ *                        previous state was. The argument is generic (void *
+ *                        pointer) that shall be casted in the state action
+ *                        callback. Currently, only char array arguments are
+ *                        implemented.
  *          Delay action: It is a special callback of the transition delay and
  *                        serves a practical purpose, for instance clearing a
  *                        LCD display during a state transition. It is run at
@@ -64,32 +72,47 @@
 class TFSM
 {
   public:
-    typedef void (*state_fp)(void);
+    typedef void (*state_action_fp)(void * action_arg);
+    typedef void (*state_delay_fp)(void);
     typedef struct {
       uint32_t cycle;
       int32_t steps;
       int16_t delay;
       uint8_t primary_transition;
       uint8_t alternate_transition;
-      state_fp action;
-      state_fp delay_cb;
+      state_action_fp action;
+      void * action_arg;
+      state_delay_fp delay_cb;
     } ST_STATE;
     TFSM(ST_STATE pStates[], size_t size);
     ~TFSM();
-    void init(void);
-    void init(ST_STATE state);
     void run(void);
     uint32_t get_current_cycle(void);
     int32_t get_current_steps(void);
-    void set_alt_transition(void);
     void force_transition(void);
+    void set_alt_transition(void);
     void set_delay(int16_t delay);
+    void set_action_arg(const char str_action_arg[33]);
+    void set_all(
+      bool alt_transition=false,
+      int16_t delay=-1,
+      bool force_transition=false,
+      const char *str_action_arg=NULL
+    );
 
   private:
+    typedef enum {
+      _E_ARG_TYPE_NONE = 0,
+      _E_ARG_TYPE_CHAR_ARRAY = 1
+    } _E_ARG_TYPE;
+    void _init(void);
+    void _init(ST_STATE state);
     ST_STATE *_pStates;
     size_t _n;
     size_t _size;
     ST_STATE _state;
+    _E_ARG_TYPE _action_arg_type;
+    char _str_action_arg[33];
     bool _alt_transition;
 };
 
